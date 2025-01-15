0db644ad44479b17f85b2a01fd825934290ea295
ascogeno <ros21045@byui.edu>
2025-01-14 23:04:06 -0700

    finshed prep 3

--- csharp-prep/Prep3/Program.cs
+++ csharp-prep/Prep3/Program.cs
@@ -4,6 +4,29 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep3 World!");
+        Random randy = new Random();
+        string answer = "";
+        Console.WriteLine("Welcome to the number-guessing game! We're currently workshopping a better name for this.");
+        do
+        {
+            int randomNumber = randy.Next(1, 100);
+            int guess;
+            do
+            {
+                Console.WriteLine("What is your guess?");
+                guess = int.Parse(Console.ReadLine());
+                if (guess < randomNumber)
+                {
+                    Console.WriteLine("Higher");
+                }
+                else if (guess > randomNumber)
+                {
+                    Console.WriteLine("Lower");
+                }
+            } while (guess != randomNumber);
+            Console.WriteLine("You guessed it!");
+            Console.WriteLine("Do you want to play again? (y/n)");
+            answer = Console.ReadLine();
+        } while (answer == "yes");
     }
 }
