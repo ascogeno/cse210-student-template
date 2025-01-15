dd8c2e831c7274d4bf73445371dfc7bbe07bb42d
ascogeno <ros21045@byui.edu>
2025-01-15 00:07:32 -0700

    finished preps 4 and 5

--- csharp-prep/Prep4/Program.cs
+++ csharp-prep/Prep4/Program.cs
@@ -1,9 +1,36 @@
 using System;
+using System.Diagnostics;
+using System.Collections.Generic;
 
 class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep4 World!");
+        Console.WriteLine("Enter a list of numbers, type 0 when finished.");
+        int userInput;
+        List<int> list = new List<int>();
+        do
+        {
+            Console.Write("Enter number: ");
+            userInput = int.Parse(Console.ReadLine());
+            list.Add(userInput);
+        } while (userInput != 0);
+
+        int sum = 0;
+        double average;
+        int max = int.MinValue;
+        for (int i = 0; i < list.Count; i++)
+        {
+            sum = sum + list[i];
+            if (list[i] > max)
+            {
+                max = list[i];
+            }
+        }
+        average = (double)sum / (list.Count-1);
+
+        Console.WriteLine($"The sum is {sum}");
+        Console.WriteLine($"The average is {average}");
+        Console.WriteLine($"The largest number is {max}");
     }
 }
--- csharp-prep/Prep5/Program.cs
+++ csharp-prep/Prep5/Program.cs
@@ -4,6 +4,40 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep5 World!");
+        DisplayWelcome();
+        string name = PromptUserName();
+        int number = PromptUserNumber();
+        int squared = SquareNumber(number);
+        string squaredString = squared.ToString();
+        DisplayResult(name,squaredString);
+    }
+
+    static void DisplayWelcome()
+    {
+        Console.WriteLine("Welcome to the Program!");
+    }
+
+    static string PromptUserName()
+    {
+        //asks the user for their name
+        Console.Write("Please enter your name: ");
+        return Console.ReadLine();
+    }
+
+    static int PromptUserNumber()
+    {
+        //asks the user for their favorite number (for phishing purposes, obviously)
+        Console.Write("Please enter your favorite number: ");
+        return int.Parse(Console.ReadLine());
+    }
+
+    static int SquareNumber(int toBeSquared)
+    {
+        return toBeSquared*toBeSquared;
+    }
+
+    static void DisplayResult(string name, string numberString)
+    {
+        Console.WriteLine($"{name}, the square of your number is {numberString}");
     }
 }
